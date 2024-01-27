68513ce5bee855ad492fd826052aa9352a64872e
Vinicius <viniciuszaneti.vz@gmail.com>
2024-01-26 21:51:35 -0300

    finalizando integracao com api

--- VSConnect_Kotlin_code-main/app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
+++ VSConnect_Kotlin_code-main/app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
@@ -3,24 +3,46 @@ package com.senai.vsconnect_kotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import com.senai.vsconnect_kotlin.databinding.ActivityLoginBinding
+import com.senai.vsconnect_kotlin.models.Login
 
 class LoginActivity : AppCompatActivity() {
 
+    //É uma propriedade privada  como o nome binding do tipo ActivityLoginBinding
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-    binding = ActivityLoginBinding.inflate(layoutInflater)
+        //Atribui à variável binding um objeto que contém referências (propriedades) aos elementos definidos no layout
+        binding = ActivityLoginBinding.inflate(layoutInflater)
 
-    binding.btnEntrar.setOnClickListener {
-        val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
-
+        //setOnClickListener é um ouvinte de clique
+        //Ou seja, quando clicar no botão entrar irá cair nesse bloco
+        binding.btnEntrar.setOnClickListener {
+            //variável mainIntent com a intenção de sair da LoginActivity e ir para MainActivity
+            val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+            //executa a intenção armazenada na variável mainIntent
             startActivity(mainIntent)
-
+            //finaliza a LoginActivity
             finish()
-    }
+        }
         setContentView(binding.root)
+
     }
+
+private fun autenticarUsuario(){
+    val root: View = binding.root
+
+    val idEmail = root.findViewById<EditText>(R.id.campo_email)
+    val idSenha = root.findViewById<EditText>(R.id.campo_senha)
+
+    val emailDigitado = idEmail.text.toString()
+    val senhaDigitada = idSenha.text.toString()
+
+    val usuario = Login(emailDigitado, senhaDigitada)
+}
+
 }
